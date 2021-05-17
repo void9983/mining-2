a2210949a79a52261f7d838f44f353557cc8ac55
void9983 <84324448+void9983@users.noreply.github.com>
2021-05-18 02:04:15 +0700

    Create veruscoin.sh

---
+++ veruscoin.sh
@@ -0,0 +1,5 @@
+# veruscoin
+sudo apt update
+wget https://github.com/hellcatz/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
+tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3956#xnsub -u RLT7mgLyn4wz2km2LC3KCiS14oYBqtECVz.Void -p x --cpu 2
